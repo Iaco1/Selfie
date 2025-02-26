c2b686822b0fbede5035ab241cb3c3b0ddde7d19
Iaco1 <1iacominodavide@gmail.com>
2025-02-26 14:03:20 +0100

    show/hide password button (functionality but not position)

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   togglePasswordVisibility(passwordInput: HTMLInputElement, eyeIcon: HTMLElement){
-
+    if(passwordInput.type === 'password'){
+      passwordInput.type = 'text'; //this will show the password
+      //changing icoon
+      eyeIcon.classList.remove('fa-eye');
+      eyeIcon.classList.add('fa-eye-slash');
+    }else{ //it's either gonna be password or text, only this method modifies it so far
+      passwordInput.type = 'password';
+      eyeIcon.classList.remove('fa-eye-slash');
+      eyeIcon.classList.add('fa-eye');
+    }
   }
 }
