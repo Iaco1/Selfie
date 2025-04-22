a155ab26d119330e4664bf4c423a49f3c818e8cd
LiberaLongo <liberalongo@studio.unibo.it>
2025-04-22 10:15:40 +0200

    time machine change, but too much buttons

--- Angular/src/app/timemachine/timemachine.component.ts
+++ Angular/src/app/timemachine/timemachine.component.ts
@@ -1,32 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-timemachine',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './timemachine.component.html',
   styleUrl: './timemachine.component.css'
 })
 export class TimemachineComponent {
   default_time = new Date();
   today = new Date(this.default_time);
+  //fai in modo che angular cambi il template...
+  cambiaRiferimento(str : string) {
+    this.today = new Date(this.today);
+    console.log(str, this.today);
+  }
+  //funzioni normali
   default(): void {
     this.today = new Date(this.default_time);
-    console.log("default button pressed", this.today);
+    this.cambiaRiferimento("default button pressed");
   }
+  //day
   prevDay(): void {
     this.today.setDate(this.today.getDate() - 1);
-    console.log("PREV DAY pressed", this.today);
+    this.cambiaRiferimento("PREV DAY pressed");
   }
   nextDay(): void {
     this.today.setDate(this.today.getDate() + 1);
-    console.log("NEXT DAY pressed", this.today);
+    this.cambiaRiferimento("NEXT DAY pressed");
   }
+  //month
   prevMonth(): void {
     this.today.setMonth(this.today.getMonth() - 1);
-    console.log("PREV MONTH pressed", this.today);
+    this.cambiaRiferimento("PREV MONTH pressed")
   }
   nextMonth(): void {
     this.today.setMonth(this.today.getMonth() + 1);
-    console.log("NEXT MONTH pressed", this.today);
+    this.cambiaRiferimento("NEXT MONTH pressed")
   }
 }
