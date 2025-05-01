a7fb3a8e17e24f6d1ce0346da180c8e748813637
LiberaLongo <liberalongo@studio.unibo.it>
2025-05-02 01:51:03 +0200

    event editable

--- Angular/src/app/calendar/event/event.component.ts
+++ Angular/src/app/calendar/event/event.component.ts
@@ -1,50 +1,70 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-event',
-	imports: [CommonModule],
+	imports: [CommonModule, FormsModule],
 	templateUrl: './event.component.html',
 	styleUrl: './event.component.css'
 })
 export class EventComponent {
-	//evento Dummy
 	event = {
-		id: "mongoDB choose me :P",
-		note: "go to luigi",
-		dateFrom: new Date(),
-		dateTo: new Date(),
+	id: 'mongoDB choose me :P',
+	info: {
+		note: 'go to luigi',
 		description: "go to luigi's house at 10",
 		location: "luigi's house",
-		who: [
-				{id:0, name:"its me Mario!"},
-				{id:1, name:"Toad"}
-			],
-		repeat: [
-				{id:0, time:"5 min"},
-				{id:1, time:"1 hour"},
-				{id:2, time:"1 day"},
-				{id:3, time:"2 day"},
-				{id:4, time:"1 week"}
-			],
-		colour: "green",
-		emoji: ":3"
-	}
-	constructor() {};
+		colour: 'green',
+		emoji: ':3'
+	},
+	dateFrom: new Date(),
+	dateTo: new Date(),
+	who: [
+		{ id: 0, name: 'its me Mario!' },
+		{ id: 1, name: 'Toad' }
+	],
+	repeat: [
+		{ id: 0, time: '5 min' },
+		{ id: 1, time: '1 hour' },
+		{ id: 2, time: '1 day' },
+		{ id: 3, time: '2 day' },
+		{ id: 4, time: '1 week' }
+	]
+	};
 
-	editDescription() {
-		console.log("edit Description not implemented yet");
+	infos: any[] = [];
+
+	constructor() {
+		this.infos = Object.entries(this.event.info)
+			.map(([key, value], index) =>
+			({
+				id: index,
+				name: key,
+				value: value,
+				hidden: true
+			}));
 	}
+
+	toggle(info: any) {
+		info.hidden = !info.hidden;
+	}
+
+	edit(info: any) {
+		info.hidden = true;
+		// This is the assertion TypeScript needs:
+		this.event.info[info.name as keyof typeof this.event.info] = info.value;
+	}
+
+	trackById(index: number, item: any) {
+		return item.id;
+	}
+
 	editDateFrom() {
-		console.log("edit Date FROM not implemented yet");
+		console.log('edit date from not implemented yet');
 	}
+
 	editDateTo() {
-		console.log("edit Date TO not implemented yet");
-	}
-	editLocation() {
-		console.log("edit Location not implemented yet");
-	}
-	editEmoji() {
-		console.log("edit Emoji not implemented yet");
+		console.log('edit date to   not implemented yet');
 	}
 }
