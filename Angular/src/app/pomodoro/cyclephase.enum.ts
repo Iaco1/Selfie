08492497882e16baec9c64a70f600608e309572d
Iaco1 <1iacominodavide@gmail.com>
2025-07-22 12:11:02 +0200

    hellish logic rebuild, buttons work for frontend functionalities

--- Angular/src/app/pomodoro/cyclephase.enum.ts
+++ Angular/src/app/pomodoro/cyclephase.enum.ts
@@ -1,5 +1,7 @@
 export enum CyclePhase {
   IDLE = "IDLE",
+  READY = "READY",
+  SET = "SET",
   STUDYING = "STUDYING",
-  RESTING = "RESTING"
+  RESTING = "RESTING",
 }
--- Angular/src/app/pomodoro/pomodoro.component.ts
+++ Angular/src/app/pomodoro/pomodoro.component.ts
@@ -3,7 +3,7 @@ import {CyclePhase} from './cyclephase.enum';
 import {Router} from "@angular/router";
 import {FormsModule} from '@angular/forms';
 import {TimeMachineService} from '../services/time-machine.service';
-import {take, takeWhile} from 'rxjs';
+import {finalize, take, takeWhile} from 'rxjs';
 
 @Component({
   selector: 'app-pomodoro',
@@ -14,196 +14,249 @@ import {take, takeWhile} from 'rxjs';
   styleUrl: './pomodoro.component.css'
 })
 export class PomodoroComponent {
-  studyTimeInputHidden = false;
   cyclephase = CyclePhase.IDLE;
-  breakTimeHidden = false;
-  breakAnimationActive = false;
-  studyAnimationActive = false;
-  repetitionsHidden = true;
   cycleButton = "start cycle";
-  nextPhase= "";
-  intervalsSet = false;
-  repetitionsSelectorHidden = true;
 
-  sessionTime = {h: 0, m: 35, s: 0};
-  pomodoroTime = {h: 0, m: 30, s: 0};
-  breakTime = {h: 0, m: 5, s: 0};
-  manualRepetitions = 5;
-  repetitions = 5;
-  sessionSecondsLeft = 0;
+
+  //countdowns
   pomodoroSecondsLeft = 0;
   breakSecondsLeft = 0;
+  pomodoroTime = {h: 0, m: 25, s: 0};
+  breakTime = {h: 0, m: 5, s: 0};
 
-  // in seconds
-  pomodoroDuration = {h: 0, m: 30, s: 0};
+  //session durations
+  repetitions = 3;
+  sessionTime = {h: 1, m: 30, s: 0};
+
+  //cycle durations
+  pomodoroDuration = {h: 0, m: 25, s: 0};
   breakDuration = {h: 0, m: 5, s: 0};
+
 
   pomodoro = {startTime: new Date(Date.UTC(2000)), endTime: new Date(Date.UTC(2001)), duration: 1, competionStatus: false, authorId: ""};
 
   constructor(private router: Router, private timemachine: TimeMachineService) {}
 
+  resetCountdowns(){
+    this.pomodoroSecondsLeft = this.pomodoroDuration.h*3600 + this.pomodoroDuration.m*60;
+    this.breakSecondsLeft = this.breakDuration.m*60 + this.breakDuration.s;
+    this.pomodoroTime = this.pomodoroDuration;
+    this.breakTime = this.breakDuration;
+  }
 
-  formatTime(seconds: number) {
+  resetDefaultCountdowns(){
+    this.pomodoroSecondsLeft = 1500;
+    this.breakSecondsLeft = 300;
+    this.pomodoroDuration = {h: 0, m: 25, s: 0};
+    this.breakDuration= {h: 0, m: 5, s: 0};
+  }
+
+  formatTimeInSeconds(time: {h: number, m: number, s: number}){
+    return time.h*3600 + time.m*60 + time.s;
+  }
+
+  formatTimeInHMS(seconds: number) {
     const hrs = Math.floor(seconds / 3600);
     const mins = Math.floor((seconds % 3600) / 60);
     const secs = (seconds % 60);
     return {h: hrs, m: mins, s: secs};
   }
 
+  setRepetitions(override: boolean, manual: boolean = false){
+    if(this.cyclephase !== CyclePhase.SET || override) this.autoComputeRepetitions(manual);
+    else return;
+  }
 
-  autoComputeCycles(manual: boolean = false) {
-    // computing in seconds
-    let sTs : number = this.sessionTime.h*3600 + this.sessionTime.m*60;
-    let pTs : number = this.pomodoroTime.h*3600 + this.pomodoroTime.m*60;
-    let bTs : number = this.breakTime.m*60 + this.breakTime.s;
 
+  autoComputeRepetitions(manual: boolean = false) {
     if(manual) {
-      this.repetitions = this.manualRepetitions
-      sTs = (pTs + bTs) * this.repetitions;
+      const sessionSeconds = this.repetitions * (this.pomodoroDuration.h*3600 + this.pomodoroDuration.m*60 + this.breakDuration.m*60 + this.breakDuration.s);
+      this.sessionTime = this.formatTimeInHMS(sessionSeconds)
     }else{
+      // computing in seconds
+      let sTs : number = this.sessionTime.h*3600 + this.sessionTime.m*60;
+      let pTs : number = this.pomodoroDuration.h*3600 + this.pomodoroDuration.m*60;
+      let bTs : number = this.breakDuration.m*60 + this.breakDuration.s;
       this.repetitions = Math.floor(sTs / (pTs + bTs));
     }
-
-
-    this.sessionSecondsLeft = sTs;
-    this.pomodoroSecondsLeft = pTs;
-    this.breakSecondsLeft = bTs;
-    this.pomodoroDuration = this.pomodoroTime;
-    this.breakDuration = this.breakTime;
+    this.cyclephase = CyclePhase.SET;
   }
 
   emitNotification(){
     //this.router.navigate([{outlets: {asideRight: 'NotificationComponent'} }]);
   }
 
-  startCycle(){
-    this.nextPhase = "break";
-    this.autoComputeCycles(true);
-    this.resumeCycle();
-    this.emitNotification();
+
+  readyCycle(){
+    this.cyclephase = CyclePhase.READY;
+    this.resetCountdowns();
+    this.setCycleButton();
+    this.setRepetitions(false, true);
   }
 
-  resumeCycle(){
-    this.cycleButton = "pause cycle";
-    this.nextPhase = "break";
-    this.cyclephase = CyclePhase.STUDYING;
-    this.breakTimeHidden = true;
-    this.breakAnimationActive = false;
-    this.studyAnimationActive = true;
-    this.repetitionsHidden = false;
-    this.repetitionsSelectorHidden = true;
 
+  startCycle(){
+    this.setRepetitions(false);
+    this.resumeCycle();
+    if(this.pomodoroSecondsLeft > 0) this.tickPomodoro();
+  }
+
+
+  resumeCycle(){
+    this.cyclephase = CyclePhase.STUDYING;
+    this.setCycleButton();
+  }
+
+
+  pauseCycle(){
+    this.resumePause();
+    if( this.breakSecondsLeft > 0) this.tickBreak();
+  }
+
+  resumePause(){
+    this.cyclephase = CyclePhase.RESTING;
+    this.setCycleButton();
+  }
+
+
+  toggleCycle() {
+    if(this.cyclephase == CyclePhase.READY || this.cyclephase == CyclePhase.SET || this.cyclephase == CyclePhase.RESTING) this.startCycle();
+    else if(this.cyclephase == CyclePhase.STUDYING) this.pauseCycle();
+  }
+
+
+  endSession() {
+    this.cyclephase = CyclePhase.IDLE;
+    this.resetDefaultCountdowns();
+  }
+
+
+  skipToNextCycle(){
+    if(this.repetitions > 0){
+      this.repetitions--;
+      this.readyCycle();
+    }else{
+      this.endSession();
+    }
+  }
+
+  restartPomodoro(){
+    this.readyCycle();
+  }
+
+  hideTimeInputs(){
+    return this.cyclephase !== CyclePhase.IDLE;
+  }
+
+  hideCountdowns(){
+    return this.cyclephase === CyclePhase.IDLE;
+  }
+
+  runBreakAnimation(){
+    return this.cyclephase === CyclePhase.RESTING;
+  }
+
+  runStudyAnimation(){
+    return this.cyclephase === CyclePhase.STUDYING;
+  }
+
+  hideBreakAnimation(){
+    return this.cyclephase !== CyclePhase.RESTING;
+  }
+  hideStudyAnimation(){
+    return this.cyclephase !== CyclePhase.STUDYING;
+  }
+
+  hideRepetitionsLeft(){
+    return this.cyclephase === CyclePhase.IDLE || this.cyclephase === CyclePhase.READY || this.cyclephase === CyclePhase.SET;
+  }
+
+  hideSetIntervals(){
+    return this.cyclephase !== CyclePhase.IDLE;
+  }
+
+  hideMainInterface(){
+    return this.cyclephase === CyclePhase.IDLE;
+  }
+
+  hideRestart(){
+    return this.cyclephase !== CyclePhase.STUDYING && this.cyclephase !== CyclePhase.RESTING;
+  }
+
+  hideRepetitionSelector(){
+    return this.cyclephase !== CyclePhase.READY && this.cyclephase !== CyclePhase.SET;
+  }
+
+  hideSkip(){
+    return this.cyclephase !== CyclePhase.STUDYING && this.cyclephase !== CyclePhase.RESTING;
+  }
+
+  logStartTime(){
     this.timemachine.day$.pipe(take(1)).subscribe(
       (day) => {
         console.log("startTime: " + day);
         this.pomodoro.startTime = day;
       });
+  }
+
+  tickPomodoro(){
     this.timemachine.day$.pipe(
-      takeWhile(() => this.pomodoroSecondsLeft > 0 && this.cyclephase == CyclePhase.STUDYING)).subscribe(
+      takeWhile(() => this.pomodoroSecondsLeft > 0 && this.cyclephase == CyclePhase.STUDYING),
+      finalize(() => {
+        if(this.cyclephase == CyclePhase.RESTING || this.cyclephase == CyclePhase.IDLE || this.cyclephase == CyclePhase.READY || this.cyclephase == CyclePhase.SET) return;
+        else if(this.breakSecondsLeft > 0) this.pauseCycle();
+        else this.skipToNextCycle();
+      })
+    ).subscribe(
       (day) => {
-        this.sessionSecondsLeft--;
         this.pomodoroSecondsLeft--;
-        this.sessionTime = this.formatTime(this.sessionSecondsLeft);
-        this.pomodoroTime = this.formatTime(this.pomodoroSecondsLeft);
-      },
-      () => {
-        if(this.cyclephase == CyclePhase.STUDYING) this.pauseCycle(true);
+        this.pomodoroTime = this.formatTimeInHMS(this.pomodoroSecondsLeft);
       }
     )
   }
 
-  pauseCycle(startBreak: boolean = true){
-    this.cyclephase = CyclePhase.IDLE;
-    this.nextPhase = "study";
-    this.cycleButton = "resume cycle";
-    this.studyAnimationActive = false;
-
-    if(startBreak){
-      this.cyclephase = CyclePhase.RESTING;
-      this.breakTimeHidden = true;
-      this.breakAnimationActive = true;
-    }
-
-
+  logEndTime(){
     this.timemachine.day$.pipe(take(1)).subscribe(
       (day) => {
         this.pomodoro.endTime = day;
         console.log("endTime: " + day);
       }
     )
-    this.timemachine.day$.pipe(takeWhile(() => this.breakSecondsLeft > 0 && this.cyclephase == CyclePhase.RESTING)).subscribe(
+  }
+
+  tickBreak(){
+    this.timemachine.day$.pipe(
+      takeWhile(() => this.breakSecondsLeft > 0 && this.cyclephase == CyclePhase.RESTING),
+      finalize(() => {
+        if(this.cyclephase == CyclePhase.STUDYING || this.cyclephase == CyclePhase.IDLE || this.cyclephase == CyclePhase.READY || this.cyclephase == CyclePhase.SET) return;
+        else if(this.pomodoroSecondsLeft > 0) this.startCycle();
+        else if(this.repetitions > 0) this.skipToNextCycle();
+        else this.endSession()
+      })
+      ).subscribe(
       (day) => {
         this.breakSecondsLeft--;
-        this.sessionSecondsLeft--;
-        this.sessionTime = this.formatTime(this.sessionSecondsLeft);
-        this.breakTime = this.formatTime(this.breakSecondsLeft);
-      },
-      () => {
-        if(this.cyclephase == CyclePhase.RESTING) this.resumeCycle();
+        this.breakTime = this.formatTimeInHMS(this.breakSecondsLeft);
       }
     )
   }
 
-
-  toggleCycle() {
-    if(this.cyclephase == CyclePhase.IDLE || this.cyclephase == CyclePhase.RESTING) {
-      this.startCycle();
-    }else if(this.cyclephase == CyclePhase.STUDYING){
-      this.pauseCycle(true);
-    }
-    this.studyTimeInputHidden = true;
-  }
-
-  endSession() {
-    this.nextPhase = "";
-    this.studyTimeInputHidden = false;
-    this.cyclephase = CyclePhase.IDLE;
-    this.cycleButton = "start cycle";
-    this.studyAnimationActive = false;
-    this.breakAnimationActive = false;
-    this.breakTimeHidden = false;
-    this.repetitionsHidden = true;
-    this.pomodoroTime = this.pomodoroDuration;
-    this.breakTime = this.breakDuration;
-    this.intervalsSet = !this.intervalsSet;
-  }
-
-  skipToNextCycle(){
-    console.log("repetitions:" + this.repetitions);
-    if(this.repetitions <=0){
-      this.endSession();
-      console.log("session ended");
-      return;
-    }
-
-    switch (this.cyclephase) {
-      case CyclePhase.RESTING:
-        this.cyclephase = CyclePhase.RESTING;
-        this.pauseCycle(false);
+  setCycleButton(){
+    switch(this.cyclephase){
+      case CyclePhase.READY:
+      case CyclePhase.SET:
+        this.cycleButton = "start cycle";
         break;
       case CyclePhase.STUDYING:
-        this.cyclephase = CyclePhase.RESTING;
-        this.pauseCycle(false); // i need a new method for stopping the pausing the cycle but not starting the breaktime
-        //this.resumeCycle();
+        this.cycleButton = "pause cycle";
         break;
-      case CyclePhase.IDLE:
+      case CyclePhase.RESTING:
+        this.cycleButton = "resume cycle";
+        break;
+      default:
         break;
     }
-    this.repetitions--;
-    this.pomodoroTime = this.pomodoroDuration;
-    this.breakTime = this.breakDuration;
   }
 
-  setIntervals(){
-    this.intervalsSet = !this.intervalsSet;
-    this.studyTimeInputHidden = !this.studyTimeInputHidden;
-    this.breakTimeHidden = !this.breakTimeHidden;
-    this.repetitionsSelectorHidden = false;
-  }
-
-  restartPomodoro(){
-    this.pauseCycle(false);
-    this.pomodoroTime = this.pomodoroDuration;
-    this.breakTime = this.breakDuration;
-  }
+  protected readonly CyclePhase = CyclePhase;
 }
