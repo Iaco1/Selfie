ecc2a5aae7ea1bca636e38131d390c1254cd4b60
Iaco1 <1iacominodavide@gmail.com>
2025-07-23 18:56:57 +0200

    preview of last pomodoro and placeholder for calendar events preview

--- Angular/src/app/homepage/homepage.component.ts
+++ Angular/src/app/homepage/homepage.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import {PomodoroService} from '../services/pomodoro.service';
+import {DecimalPipe} from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [
+    DecimalPipe
+  ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
+  lastPomodoro: any;
+  startTime = "last pomodoro fetch failed or is yet to happen";
+  duration = 0;
 
+  constructor(private pomodoroService: PomodoroService) {
+    this.pomodoroService.get(localStorage.getItem('authToken')!).subscribe({
+      next: (response) => {
+        console.log("get pomodoros result: ", response);
+        this.lastPomodoro = response.pomodoro.at(-1);
+        this.startTime = this.lastPomodoro.startTime;
+        this.duration = this.lastPomodoro.duration;
+      },
+      error: (error) => {
+        console.log("get pomodoros failed: ", error);
+      }
+    });
+  }
+
+  getLastPomodoro(){
+
+  }
 }
