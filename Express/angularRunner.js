1216b6a5b2543db214607b1ca0d17d7219d015e8
Iaco1 <1iacominodavide@gmail.com>
2025-07-29 14:56:43 +0200

    https. no local network access

--- Express/angularRunner.js
+++ Express/angularRunner.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const https = require('https');
+const fs = require('fs');
 
+// Define paths for your SSL certificate and key
+const sslKeyPath = path.join(__dirname, '../Angular/cert-key.pem');
+const sslCertPath = path.join(__dirname, '../Angular/cert.pem');
 
+// Read SSL certificate and key files
+const sslKey = fs.readFileSync(sslKeyPath, 'utf8');
+const sslCert = fs.readFileSync(sslCertPath, 'utf8');
 
 // Serve static files from the Angular build directory
-// Assuming your Angular build output is in the 'dist' folder
 const staticPath = path.join(__dirname, '../Angular/dist/angular/browser');
 console.log('Serving static files from:', staticPath);
 
@@ -17,7 +24,15 @@ app.use((req, res) => {
     res.sendFile(path.join(staticPath, 'index.html'));
 });
 
+// Create the HTTPS server
+const httpsOptions = {
+    key: sslKey,
+    cert: sslCert,
+};
+
+
 const port = 4200;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+https.createServer(httpsOptions, app).listen(port, () => {
+    console.log(`Server running on https://localhost:${port}`);
 });
+
